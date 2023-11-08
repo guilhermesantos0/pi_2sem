0ada419e93e14edfb92123c4f59889feb67fdd99
guilhermesantos0 <guilhermesantosdev@outlook.com>
2023-11-07 23:09:06 -0300

    pagina de contato

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,11 +1,36 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+import "../styles/pages/Contato.css"
+
 function Contato () {
     return(
         <div className="Contato">
             <Navbar tab="Contato" />
-            <h1>Fale conosco</h1>
+                <div className="header-page">
+                    <div className="color">
+                        <h1 className="word"><b>FALE CONOSCO</b></h1>
+                    </div>
+                </div>
+                
+                <div className="body">
+                    <div class="image">
+                        <p class="imagedesc"><b>Estamos aqui para você</b></p>
+                    </div>
+                    <div className="contatos">
+                        <div class="header">
+                            <p class="word3"><b>Canais de Contato:</b></p>
+                        </div>
+
+                        <div className="box">
+                            <div className="colunas" style={{textAlign: "center"}}>
+                                <div class="col"><p>Contato:<br/><br/>Whatsapp:91234-5678<br/>E-mail:usina@gmail.com</p></div>
+                                <div class="col"><p>Redes Sociais:<br/><br/>Instagram:usina@gmail.com<br/>Facebook:usina@gmail.com</p></div>
+                                <div class="col"><p>Apoie:<br/><br/>Whatsapp:91234-5678<br/>E-mail:usina@gmail.com</p></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             <Footer />
         </div>
     )
