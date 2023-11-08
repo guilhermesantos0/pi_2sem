470cad867bf5a74771ff88dc0be22cff91048568
guilhermesantos0 <guilhermesantosdev@outlook.com>
2023-11-08 11:03:00 -0300

    pagina contato responsiva e adicionada pagina apoie

--- src/pages/Apoie.jsx
+++ src/pages/Apoie.jsx
@@ -1,11 +1,32 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+import "../styles/pages/Apoie.css"
+
+import image from "../images/apoie/_wow.svg"
+
 function Apoie () {
     return(
         <div className="Apoie">
             <Navbar tab="Apoie" />
-            <h1>Apoie</h1>
+                <div className="header-page">
+                    <h1 className="word"><b>APOIE</b></h1>
+                </div>
+
+                <br/>
+
+                <p className="p1">Apoie o projeto da Usina Eco Cultural e contribua para a propagação da cultura.</p>
+
+                <br/>
+
+                <div className="img">  
+                    <img src={image} width="50%"/>
+                </div>
+
+                <br/>
+
+                <p className="p2">Toda e qualquer tipo de doação será muito bem-vinda, para mais informações, entre em contato através da aba <a href="">Fale Conosco.</a></p>
+
             <Footer />
         </div>
     )
--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -2,31 +2,33 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 import "../styles/pages/Contato.css"
+import image from "../images/contato/fale-conosco3.jpg"
 
 function Contato () {
     return(
         <div className="Contato">
             <Navbar tab="Contato" />
                 <div className="header-page">
-                    <div className="color">
-                        <h1 className="word"><b>FALE CONOSCO</b></h1>
-                    </div>
+                    <h1 className="word">FALE CONOSCO</h1>
                 </div>
                 
                 <div className="body">
-                    <div class="image">
-                        <p class="imagedesc"><b>Estamos aqui para você</b></p>
+                    <div className="image">
+                        <p><b>Estamos aqui para você</b></p>
                     </div>
+                    {/* <div className="image">
+                        <p className="imagedesc"><b>Estamos aqui para você</b></p>
+                    </div> */}
                     <div className="contatos">
-                        <div class="header">
-                            <p class="word3"><b>Canais de Contato:</b></p>
+                        <div className="header">
+                            <p className="word3"><b>Canais de Contato:</b></p>
                         </div>
 
                         <div className="box">
                             <div className="colunas" style={{textAlign: "center"}}>
-                                <div class="col"><p>Contato:<br/><br/>Whatsapp:91234-5678<br/>E-mail:usina@gmail.com</p></div>
-                                <div class="col"><p>Redes Sociais:<br/><br/>Instagram:usina@gmail.com<br/>Facebook:usina@gmail.com</p></div>
-                                <div class="col"><p>Apoie:<br/><br/>Whatsapp:91234-5678<br/>E-mail:usina@gmail.com</p></div>
+                                <div className="col"><p>Contato:<br/><br/>Whatsapp:91234-5678<br/>E-mail:usina@gmail.com</p></div>
+                                <div className="col"><p>Redes Sociais:<br/><br/>Instagram:usinaecocultural<br/>Facebook:Usina EcoCultural</p></div>
+                                <div className="col"><p>Apoie:<br/><br/>Whatsapp:91234-5678<br/>E-mail:usina@gmail.com</p></div>
                             </div>
                         </div>
                     </div>
